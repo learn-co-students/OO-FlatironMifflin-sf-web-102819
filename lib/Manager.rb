ffdd186a7cca621908ea3362f0d6c9c03a54074c
Allen Shin <geeuho@gmail.com>
2019-11-04 17:30:22 -0800

    Allen's solution

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,38 @@
 class Manager
+    @@all = []
+    attr_accessor :name, :department, :age, :employees
 
+    def initialize (name, department, age, employees)
+        @name = name
+        @department = department
+        @age = age
+        @employees = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def hire_employee(name, salaray)
+        @employees << Employee.new(name, salary, self)
+    end
+    
+    def self.all_departments
+        @@all.map { |manager|
+            manager.department
+        }
+    end
+
+    def self.average_age
+        # all_ages = @@all.map { |manager|
+        #     manager.age
+        # }
+        # total_ages = all_ages.reduce { |sum, n|
+        #     sum + n
+        # }
+        total_ages = Manager.all.reduce(0) { |sum, n|
+            sum + n.age
+        }
+    end
 end
--- run.rb
+++ run.rb
@@ -3,8 +3,13 @@ require_relative "lib/Employee"
 require 'pry'
 
 
-#Test your code here
+manager1 = Manager.new("Allen", "programming", 27, [])
+manager2 = Manager.new("Allen", "programming", 30, [])
+manager3 = Manager.new("Allen", "programming", 33, [])
 
+employee1 = Employee.new("Allen", 900, manager1)
+employee2 = Employee.new("Allen", 2000, manager1)
+employee3 = Employee.new("Allen", 45600, manager1)
 
 binding.pry
 puts "done"
